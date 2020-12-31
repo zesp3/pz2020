1d7239c0a587f7978d2be5fe677e17a25ba7f64b
sado324 <74790697+sado324@users.noreply.github.com>
2020-12-31 15:07:20 +0100

    Update kontakty.py

--- crawlery/kontakty.py
+++ crawlery/kontakty.py
@@ -1,16 +1,14 @@
 import scrapy
+from kontakt.items import KontaktItem
 
-from scrapy import cmdline
+
 class KontaktySpider(scrapy.Spider):
     name = 'kontakty'
     allowed_domains = ['lubianka.pl/9478,urzad-gminy']
     start_urls = ['https://www.lubianka.pl/9478,urzad-gminy']
 
     def parse(self, response):
-        for i in response.css('div.system_anchor'):
-             yield {
-                        'text': i.css('span::text').getall(),
-            }
-
-
-cmdline.execute("scrapy crawl kontakty -o kontakty.json -t json".split())
+        quote = response.xpath('//div[@id="akapit_29685"]')
+        item_zawartosc = quote.css('span::text').getall(),
+        kontaktItem = KontaktItem(zawartosc = item_zawartosc)
+        yield kontaktItem
